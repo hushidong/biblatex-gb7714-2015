d4585b3588066cc37409306a2a4af60da5b8194f
hzzmail@163.com <hzzmail@sjtu.org>
2016-12-03 17:47:21 +0800

    Add files via upload

---
+++ gbtclear.bat
@@ -0,0 +1,3 @@
+@echo off
+del /q *.aux *.bbl *.blg *.log *.out *.toc *.bcf *.xml *.synctex *.nlo *.nls *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.userbak 
+del /q *.nav *.snm *.vrb 
